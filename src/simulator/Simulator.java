f9a488362cafb5fd935530cef63e3de76055a689
Yang Tang <137018636@qq.com>
2021-08-27 18:02:19 +0800

    add TDFT decoding to Simulator.java

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,16 +1,70 @@
 package simulator;
 import java.util.Scanner;
 
+import simulator.transducer.TDFT;
+
 public class Simulator {
-    private Decoder decoder = new Decoder();
+
+    // stores the singleton instance of this class
+    private static Simulator simulator;
+
+    // stores the decoder instance
+    private Decoder decoder;
+
+    /**
+     * gets the singleton instance of this class
+     * 
+     * @return the singleton instance of this class
+     */
+    public static Simulator getInstance() {
+        return simulator;
+    }
+
+    /**
+     * gets the decoder instance
+     * 
+     * @return the decoder instance
+     */
+    public Decoder getDecoder() {
+        return this.decoder;
+    }
+
+    // mark constructor as private to prevent external instances
+    private Simulator() {
+    }
+
+    /**
+     * main entry point for the simulator
+     * 
+     * @param args command-line arguments supplied by the OS
+     * @throws SQLException
+     */
     public static void main(String[] args){
+        // initialize and run the program
+        simulator = new Simulator();
+        simulator.run();
+    }
+
+    private void run(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Please choose the type of the transducer model:\n" +
                             "1: 2DFT\n" +
-                            "2: SST\n");
-        int modelInt = sc.nextInt();
+                            "2: SST");
+        int modelInt = Integer.parseInt(sc.nextLine());
 
-        System.out.println("Please enter the encoding of transducer:\n");
+        System.out.println("Please enter the encoding of transducer:");
         String modelDesc = sc.nextLine();
+
+        switch (modelInt) {
+            case 1:
+                TDFT tdft = decoder.decodeTDFT(modelDesc);
+                break;
+        
+            default:
+                break;
+        }
+
+        System.out.println("Please enter the input string:\n");
+        String inpuString = sc.nextLine();
     }
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,5 +1,7 @@
 package test;
 
+import simulator.*;
+
 public class test {
     
 }
